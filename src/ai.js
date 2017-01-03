625fde5573afc30bf4bb4754df9192c7d251cef8
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-01-02 23:39:19 -0600

    dynamic logic for finding blocks

--- src/ai.js
+++ src/ai.js
@@ -1,39 +1,3 @@
-export function getBlocksNeeded(boardState, columns, rows) {
-  const blocksNeeded = []
-
-  // in row
-  if (boardState[0] === 'x' && boardState[2] === 'x') {
-    blocksNeeded.push(1)
-  }
-  if (boardState[3] === 'x' && boardState[5] === 'x') {
-    blocksNeeded.push(4)
-  }
-  if (boardState[6] === 'x' && boardState[8] === 'x') {
-    blocksNeeded.push(4)
-  }
-
-  // column
-  if (boardState[0] === 'x' && boardState[6] === 'x') {
-    blocksNeeded.push(3)
-  }
-  if (boardState[1] === 'x' && boardState[7] === 'x') {
-    blocksNeeded.push(4)
-  }
-  if (boardState[2] === 'x' && boardState[8] === 'x') {
-    blocksNeeded.push(5)
-  }
-
-  // diagonal
-  if (boardState[0] === 'x' && boardState[8] === 'x') {
-    blocksNeeded.push(4)
-  }
-  if (boardState[2] === 'x' && boardState[6] === 'x') {
-    blocksNeeded.push(4)
-  }
-
-  return blocksNeeded
-}
-
 export function getMatrix(board, columns, rows) {
   const matrix = []
   let column = 0
@@ -53,6 +17,77 @@ export function getMatrix(board, columns, rows) {
   return matrix
 }
 
+export function getBlocksNeeded(boardState, columns, rows) {
+  const blocksNeeded = []
+  const matrix = getMatrix(boardState, columns, rows)
+
+  // in row
+  let row = 0
+  while (row < rows) {
+    let column = 0
+    while (column < columns - 2) {
+      const index = row * columns + column
+      const cell = matrix[index]
+      if (cell.value === 'x') {
+        const right = matrix[index + 1]
+        if (right.value === null) {
+          const rightTwo = matrix[index + 2]
+          if (rightTwo.value === cell.value) {
+            blocksNeeded.push(index + 1)
+          }
+        }
+      }
+      column++
+    }
+    row++
+  }
+
+  // column
+  let column = 0
+  while (column < columns) {
+    let row = 0
+    while (row < rows - 2) {
+      const index = row * columns + column
+      const cell = matrix[index]
+      if (cell.value === 'x') {
+        const downIndex = (row + 1) * columns + column
+        const down = matrix[downIndex]
+        if (down.value === null) {
+          const downTwoIndex = (row + 2) * columns + column
+          const downTwo = matrix[downTwoIndex]
+          if (downTwo.value === cell.value) {
+            blocksNeeded.push(downIndex)
+          }
+        }
+      }
+      row++
+    }
+    column++
+  }
+
+  // diagonal
+  column = 0
+  row = 0
+  while (column < columns - 2 && row < rows - 2) {
+    const index = row * columns + column
+    const cell = matrix[index]
+    if (cell.value === 'x') {
+      const downRightIndex = (row + 1) * columns + column + 1
+      const downRight = matrix[downRightIndex]
+      if (downRight.value === null) {
+        const downRightTwoIndex = (row + 2) * columns + column + 2
+        const downRightTwo = matrix[downRightTwoIndex]
+        if (downRightTwo.value === cell.value) {
+          blocksNeeded.push(downRightIndex)
+        }
+      }
+    }
+    column++
+    row++
+  }
+
+  return blocksNeeded
+}
 export function getAiMove(boardState) {
   console.log('What should we do?')
   console.dir(boardState)
--- src/ai.test.js
+++ src/ai.test.js
@@ -23,26 +23,35 @@ describe('ai', () => {
   describe('getBlocksNeeded', () => {
     it('should work', () => {
       const columns = 3
-      const rows = 0
+      let rows = 1
       let board = ['x', null, 'x']
+
+        /*
+      rows = 3
       expect(ai.getBlocksNeeded(board, columns, rows)).toEqual([1])
+
+      rows = 2
       board = [
         'x', null, 'x',
         'x', null, 'x',
       ]
       expect(ai.getBlocksNeeded(board, columns, rows)).toEqual([1, 4])
-      board = [
-        'x', null, null,
-        null, null, null,
-        null, null, 'x',
-      ]
-      expect(ai.getBlocksNeeded(board, columns, rows)).toEqual([4])
+
+      rows = 3
       board = [
         'x', null, null,
         null, null, null,
         'x', null, null,
       ]
       expect(ai.getBlocksNeeded(board, columns, rows)).toEqual([3])
+      */
+      rows = 3
+      board = [
+        'x', null, null,
+        null, null, null,
+        null, null, 'x',
+      ]
+      expect(ai.getBlocksNeeded(board, columns, rows)).toEqual([4])
     })
   })
 })
